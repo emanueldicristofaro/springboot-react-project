fd4d46af3392c3710911ad85c2222e9f6dfba3d5
Emanuel Di Cristofaro <emanuelesposito3@gmail.com>
2022-12-21 12:22:16 -0300

    feat - 🍃 Refactor React

--- deliverys-of-movies/src/pages/home.tsx
+++ deliverys-of-movies/src/pages/home.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import reactLogo from '../assets/react.svg'
 
-function App() {
-  const [count, setCount] = useState(0)
+function home(){
+
+    const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="home">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/src/assets/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -31,4 +31,4 @@ function App() {
   )
 }
 
-export default App
+export default home
