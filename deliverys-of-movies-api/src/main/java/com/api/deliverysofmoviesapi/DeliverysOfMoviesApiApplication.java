98429308fe9f92e9e876327f4b27c65ae0a7aaa7
Emanuel Di Cristofaro <emanuelesposito3@gmail.com>
2022-12-20 21:03:22 -0300

    feat - 🍃 React and Springboot

---
+++ deliverys-of-movies-api/src/main/java/com/api/deliverysofmoviesapi/DeliverysOfMoviesApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.deliverysofmoviesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverysOfMoviesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverysOfMoviesApiApplication.class, args);
+	}
+
+}
